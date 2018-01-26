5de009c69827174bc854f9e918b85c10c50cf88f
Bin Lu <lu.bin.lu.bin@gmail.com>
2018-01-26 10:16:22 -0500

    webpack watch, patchy vs non-patchy

--- src/app.js
+++ src/app.js
@@ -1,40 +1,15 @@
-// import { Observable } from 'rxjs/Observable';
-// import 'rxjs/add/observable/interval';
-// import 'rxjs/add/observable/of';
-// import 'rxjs/add/operator/map';
-// import 'rxjs/add/operator/take';
-// import 'rxjs/add/operator/combineAll';
+// import { map, take, combineAll } from 'rxjs/operators';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+
+var button = document.querySelector('button');
+Observable.fromEvent(button, 'click')
+  .subscribe(() => console.log('Clicked!'));
+
+
+// import { fromEvent } from 'rxjs/observable/fromEvent';
 //
-// const source = Observable.interval(1000).take(2);
-//
-// const example = source.map(val =>
-//   Observable.interval(1000)
-//     .map(i => `Result (${val}): ${i}`)
-//     .take(5)
-// );
-//
-// const combined = example.combineAll();
-//
-// const subscribe = combined.subscribe(val => console.log(val));
-
-import { interval } from 'rxjs/observable/interval';
-import { map, take, combineAll } from 'rxjs/operators';
-
-const source = interval(1000).pipe(
-  take(2)
-);
-
-const example = source.pipe(
-  map(val =>
-    interval(1000).pipe(
-      map(i => `Result (${val}): ${i}`),
-      take(5)
-    )
-  )
-);
-
-const combined = example.pipe(
-  combineAll()
-);
-
-const subscribe = combined.subscribe(val => console.log(val));
+// var button = document.querySelector('button');
+// fromEvent(button, 'click')
+//   .subscribe(() => console.log('Clicked!'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/app.js',
   devtool: 'source-map',
+  watch: true,
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
