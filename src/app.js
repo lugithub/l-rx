549e2fc88e6d8317d252f86bffff7228c558c047
lubin <lu.bin.lu.bin@gmail.com>
2018-01-26 23:38:04 -0500

    empty

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import { defer } from 'rxjs/observable/defer';
-import { fromEvent } from 'rxjs/observable/fromEvent';
+import { of } from 'rxjs/observable/of';
 import { interval } from 'rxjs/observable/interval';
+import { empty } from 'rxjs/observable/empty';
+import { mergeMap } from 'rxjs/operators/mergeMap';
 
-var clicksOrInterval = defer(function () {
-  if (Math.random() > 0.5) {
-    return fromEvent(document, 'click');
-  } else {
-    return interval(1000);
-  }
-});
-
-clicksOrInterval.subscribe(x => console.log(x));
+var intervalObservable = interval(1000);
+var result = intervalObservable.pipe(
+  mergeMap(x =>
+    x % 2 === 1 ? of('a', 'b', 'c') : empty()
+  )
+);
+result.subscribe(x => console.log(x), console.log, () => console.log('complete'));
