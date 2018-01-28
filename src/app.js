e3d461b4f1645c84c3179e5f7cd1fac3ccf27be0
lubin <lu.bin.lu.bin@gmail.com>
2018-01-27 22:56:04 -0500

    exhaustMap

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import { fromEvent } from 'rxjs/observable/fromEvent';
 import { interval } from 'rxjs/observable/interval';
 
-import { concatMap, take } from 'rxjs/operators';
+import { exhaustMap, take } from 'rxjs/operators';
 
 var clicks = fromEvent(document, 'click');
 var result = clicks.pipe(
-  concatMap(ev => interval(1000).pipe(
+  exhaustMap(ev => interval(1000).pipe(
     take(3)
   ))
 );
