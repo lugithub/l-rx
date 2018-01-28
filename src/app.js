9deac1b146894e38491cc9f457dcd7c3d0822ee6
lubin <lu.bin.lu.bin@gmail.com>
2018-01-28 17:55:21 -0500

    groupBy

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
-import { fromEvent } from 'rxjs/observable/fromEvent';
-import { interval } from 'rxjs/observable/interval';
+import { of } from 'rxjs/observable/of';
+import { groupBy, mergeMap, scan, last } from 'rxjs/operators';
 
-import { exhaustMap, take } from 'rxjs/operators';
-
-var clicks = fromEvent(document, 'click');
-var result = clicks.pipe(
-  exhaustMap(ev => interval(1000).pipe(
-    take(3)
-  ))
-);
-result.subscribe(x => console.log(x));
+of({id: 1, name: 'aze1'},
+   {id: 2, name: 'sf2'},
+   {id: 2, name: 'dg2'},
+   {id: 1, name: 'erg1'},
+   {id: 1, name: 'df1'},
+   {id: 2, name: 'sfqfb2'},
+   {id: 3, name: 'qfs3'},
+   {id: 2, name: 'qsgqsfg2'}
+ ).pipe(
+   groupBy(p => p.id)
+)
+.subscribe(group => {
+  console.log(group);
+  group.pipe(
+    scan((acc, cur) => [...acc, cur], []),
+    last()
+  )
+  .subscribe(console.log);
+});
