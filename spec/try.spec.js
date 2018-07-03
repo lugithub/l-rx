bd43289b9fc65d99400bb435bdfad6f650eb2618
Bin Lu <lu.bin.lu.bin@gmail.com>
2018-07-03 15:33:36 -0400

    marble of delay

--- spec/try.spec.js
+++ spec/try.spec.js
@@ -1,19 +1,71 @@
 import { marbles } from "rxjs-marbles/jest";
-import { map } from "rxjs/operators";
+import { concat, debounceTime, delay, map, merge } from "rxjs/operators";
 
 describe("basic", () => {
 
-    it("should support marble tests without values", marbles(m => {
+    xit("should support marble tests without values", marbles(m => {
+        const a =   m.cold("--1--2--|");
+        const asub =       "^-------!";
 
-        const source =  m.hot("--^-a-b-c-|");
-        const subs =            "^-------!";
-        const expected = m.cold("--b-c-d-|");
+        const b =   m.cold(        "--3--|");
+        const bsub =       "--------^----!";
 
-        const destination = source.pipe(
-            map(value => String.fromCharCode(value.charCodeAt(0) + 1))
-        );
-        m.expect(destination).toBeObservable(expected);
-        m.expect(source).toHaveSubscriptions(subs);
+        const expected =   "--1--2----3--|";
+                    
+        const result = a.pipe(concat(b));
+        m.expect(result).toBeObservable(expected);
+        m.expect(a).toHaveSubscriptions(asub);
+        m.expect(b).toHaveSubscriptions(bsub);
     }));
 
+    xit('hot', marbles(m => {
+        const a =   m.cold("--1--2--|");
+        const asub =       "^-------!";
+
+        const b =    m.hot("^----3----4--|");
+        const bsub =       "--------^----!";
+
+        const expected =   "--1--2----4--|";
+
+        
+        
+        const result = a.pipe(concat(b));
+
+        m.expect(result).toBeObservable(expected);
+        m.expect(a).toHaveSubscriptions(asub);
+        m.expect(b).toHaveSubscriptions(bsub);
+    }));
+
+    xit('g', marbles(m => {
+        const a =   m.cold("-1------2----|");
+        const b =   m.cold("-3------4----|");
+        const c =   m.cold("-5------6----|");
+
+        const expected =   "-(135)--(246)|";
+
+        const asub =     "^------------!";
+        const bsub =     "^------------!";
+        
+        const result = a.pipe(merge(b, c));
+
+        m.expect(result).toBeObservable(expected);
+        // m.expect(a).toHaveSubscriptions(asub);
+        // m.expect(b).toHaveSubscriptions(bsub);
+    }));   
+
+
+    it('delay', marbles(m => {
+        const a =   m.cold("-1-----2---|");
+        const asub =       "^------------!";
+
+        const expected = "---1-----2---|";
+
+        const result = a.pipe(
+            delay(2)
+        );
+
+        m.expect(result).toBeObservable(expected);
+        m.expect(a).toHaveSubscriptions(asub);
+    }));      
 });
+
--- src/app.js
+++ src/app.js
@@ -1,21 +1,47 @@
-import { fromEvent, Observable, Subject, ReplaySubject, interval, timer, Subscription, of, pipe } from "rxjs";
-import { catchError, combineLatest, map, mapTo, multicast, pluck, publish, refCount, retry, scan, share, shareReplay, take, takeUntil, tap } from "rxjs/operators";
-import { create, operators } from "rxjs-spy";
-import { tag } from "rxjs/operators";
+import {
+  defer,
+  fromEvent,
+  Observable,
+  Subject,
+  ReplaySubject,
+  interval,
+  timer,
+  Subscriber,
+  Subscription,
+  throwError,
+  of ,
+  pipe
+} from "rxjs";
+import {
+  catchError,
+  combineLatest,
+  concat,
+  map,
+  mapTo,
+  merge,
+  multicast,
+  pluck,
+  publish,
+  refCount,
+  retry,
+  scan,
+  share,
+  shareReplay,
+  switchMap,
+  take,
+  takeUntil,
+  tap
+} from "rxjs/operators";
+import {
+  create,
+  operators
+} from "rxjs-spy";
+import {
+  tag
+} from "rxjs/operators";
+import { POINT_CONVERSION_COMPRESSED } from "constants";
 
-create();
-const { tag } = operators;
+interval(1000).pipe(
+  take(3)
+).forEach(console.log).then(() => console.log('all right'));
 
-const source$ = interval(1000).pipe(share(), tag('source'));
-
-const mapped$ = source$.pipe(map(x => {
-  if (x === 1) {
-    throw new Error('oops');
-  }
-  return x;
-}
-), tag('mapped')).pipe(catchError(err => of(err)));
-
-source$.subscribe(x => console.log('A', x));
-mapped$.subscribe(x => console.log('B', x));
-source$.subscribe(x => console.log('C', x));
