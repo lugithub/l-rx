89a45c184cdf7d235b7d7a6b09051d1899d7d018
Bin Lu <lu.bin.lu.bin@gmail.com>
2018-12-17 11:05:04 -0500

    ajax example

--- src/app.js
+++ src/app.js
@@ -1,59 +1,28 @@
-import {
-  EMPTY,  
-  defer,
-  fromEvent,
-  range,
-  Observable,
-  Subject,
-  ReplaySubject,
-  interval,
-  timer,
-  Subscriber,
-  Subscription,
-  throwError,
-  of,
-  pipe
-} from "rxjs";
-import {
-  isEmpty,
-  catchError,
-  combineLatest,
-  concat,
-  delay,
-  filter,
-  map,
-  mapTo,
-  merge,
-  mergeMap,
-  multicast,
-  pluck,
-  publish,
-  refCount,
-  retry,
-  scan,
-  share,
-  shareReplay,
-  switchMap,
-  take,
-  takeUntil,
-  tap,
-  toArray
-} from "rxjs/operators";
-import {
-  create,
-  operators
-} from "rxjs-spy";
-import {
-  tag
-} from "rxjs/operators";
+import { ajax } from 'rxjs/ajax';
+import { from, of } from 'rxjs';
+import { catchError, map, mergeMap, reduce } from 'rxjs/operators';
 
-const value$ = range(0, 10).pipe(
-  filter(x => x % 2 === 0),
-  map(x => x + x),
-  scan((acc, x) => acc + x, 0),
-  toArray()
+// from([1, 2, 3]).pipe(
+//   mergeMap(value => of(value * value))
+// ).subscribe(console.log);
+
+
+// const obs$ = ajax(`https://api.github.com/users?per_page=5`).pipe(
+// );
+
+// const obs$ = ajax.get(`https://api.github.com/users?per_page=5`).pipe(
+// );
+
+const obs$ = ajax.getJSON(`https://api.github.com/users?per_page=5&access_token=40bdb61d42b696353ae27cef533e53b9a8140feb`).pipe(
+  mergeMap(users => from(users)),
+  mergeMap(user => ajax.getJSON(`https://api.github.com/users/${user.login}/followers?access_token=40bdb61d42b696353ae27cef533e53b9a8140feb`)),
+  mergeMap(followers => from(followers)),
+  reduce(function(acc, val) {
+    return Object.assign({}, acc, {
+          [val.login]: val
+        });
+  }, {}),
+  catchError(e => of(e.message))
 );
 
-const value = value$.toPromise();
-value.then(x => console.log(x));
-
+obs$.subscribe(console.log);
